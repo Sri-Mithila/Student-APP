abadc6efa68c48ff664d6245e23a799a5e86b316
Uwais <muhammaduuwais132@gmail.com>
2024-10-19 15:34:19 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+      <header className=" font-bold text-5xl">
+          <p>Student Record Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
